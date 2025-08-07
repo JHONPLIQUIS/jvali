e08c643acce7e30da07fbfe1723d59b12de61450
JHONPLIQUIS <126591413+JHONPLIQUIS@users.noreply.github.com>
2025-08-07 09:52:50 -0300

    Add files via upload

---
+++ variaveis.java
@@ -0,0 +1,15 @@
+public class variaveis{
+    public static void main(String[] args){
+        //tipo nome = conteudo;
+        String meuNome = "alex";
+        meuNome += "junior";
+        int idade = 17;
+        double altura = 1.79;
+        char sexo = 'M';
+        boolean casado = false;
+
+
+        System.out.println("Meu nome é "+meuNome+" Minha idade é "+idade+" e minha altura é "+altura);
+
+    }
+}
